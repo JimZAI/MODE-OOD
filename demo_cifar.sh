f3d17e6d688573604ee3d0f2d467f1bd25305ba5
JimZAI <51895743+JimZAI@users.noreply.github.com>
2023-04-19 08:50:35 +0800

    commit

--- demo_cifar.sh
+++ demo_cifar.sh
@@ -1,2 +1,2 @@
-python CSD_STEP1.py --in-dataset CIFAR-100  --out-datasets SVHN places365 LSUN iSUN dtd --name resnet34-supcon  --model-arch resnet34-supcon --ckp "/pretrained/CL/MODE-F/Finetune_epoch_500.pth"
-python CSD_STEP2.py   --in-dataset CIFAR-100  --out-datasets SVHN places365 LSUN iSUN dtd --name resnet34-supcon  --model-arch resnet34-supcon
+python feature_collect.py.py --in-dataset CIFAR-100  --out-datasets SVHN places365 LSUN iSUN dtd --name resnet34-supcon  --model-arch resnet34-supcon --ckp "/pretrained/CL/MODE-F/Finetune_epoch_500.pth"
+python CSD.py   --in-dataset CIFAR-100  --out-datasets SVHN places365 LSUN iSUN dtd --name resnet34-supcon  --model-arch resnet34-supcon
